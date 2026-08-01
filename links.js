de0f2f304c8f8729f68c7031efca97e21a3cc5f8
Debasish Sahoo <92988516+Deba-1@users.noreply.github.com>
2026-08-01 08:55:04 +0530

    Create links.js

---
+++ links.js
@@ -0,0 +1,21 @@
+const AFFILIATE_TAG = "debasish08f-21"; //[cite: 2]
+
+const products = [
+    {
+        title: "Ganesh Vegetable Dicer & Cutter with Stainless Steel Blades",
+        price: "499",
+        mrp: "₹999",
+        discount: "50% off",
+        image: "https://m.media-amazon.com/images/I/61x-QZ3sS3L._SL1500_.jpg",
+        link: "https://www.amazon.in/dp/B01BVDS1BE?tag=debasish08f-21" //[cite: 2]
+    },
+    {
+        title: "ACTIVA Aero 1200mm Ceiling Fan with LED Light",
+        price: "1,299",
+        mrp: "₹2,499",
+        discount: "48% off",
+        image: "https://m.media-amazon.com/images/I/51Y3xN9MURL._SL1200_.jpg",
+        link: "https://www.amazon.in/dp/B0G2MGMGDV?tag=debasish08f-21" //[cite: 2]
+    }
+    // Add more product objects here following the same format
+];
